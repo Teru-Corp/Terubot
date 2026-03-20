c657d8cfd0368eeade6b179a6e71b180df7ebab5
Ryu OSADA <163379190+Ryu-292@users.noreply.github.com>
2026-03-20 14:45:18 +0100

    Delete ESP32_Eyes/Right_Eye directory

--- ESP32_Eyes/Right_Eye/behaviour.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// gaze output is normalized: -1.0 (left/up) to +1.0 (right/down)
-void behaviourActivate(char cmd);
-void behaviourUpdate(float dt);
-float behaviourGetGazeX();
-float behaviourGetGazeY();
